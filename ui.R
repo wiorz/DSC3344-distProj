200c65d137e9080aa5edcb0812be0d3dee0e8372
Ivan <kostivan@gmail.com>
2022-03-02 17:42:55 -0600

    changed Slider into input box, swapped uniform param
    the parameter of uniform distribution is not intuitive, so made a swap in the logic that the max is now that first param, and min the second.

--- ui.R
+++ ui.R
@@ -5,17 +5,15 @@ ui <- fluidPage(
     h2('Central Limit Theorem'),
     
     # instructions to use the simulation
-    helpText('Input a value for the parameters, while considering the comments 
-              under "Additional Comments". Then, click "Run Simulation" to update
-              graphs.'),
+    helpText('Input a value for the sample size and then parameters, while 
+              considering the comments under "Additional Comments". Then, click 
+              "Run Simulation" to update graphs.'),
     
     # reference
     h6(a("Referenced from: https://github.com/coursephd/week4shinyApp")),
     
     # user selection on input size 
-    sliderInput(inputId="n",
-                "Sample Size n",
-                value=30, min=1, max=2000, step=20),
+    numericInput("n","Input Sample Size (Integer)", 1),
     
     # parameters 
     numericInput("par1","First Parameter", 1),
@@ -25,7 +23,7 @@ ui <- fluidPage(
     radioButtons("src.dist", "Select Distribution:",
                  c("Normal (Mean, SD)" = "N",
                    "Exponential (Mean)" = "E",
-                   "Uniform (Min, Max)" = "U",
+                   "Uniform (Max, Min)" = "U",
                    "Binomial (Size, Success Probability)" = "B",
                    "Chi Square (Degree of Freedom,  non-centrality parameter)" = "X", 
                    "Student t (Degree of Freedom)" = "T",
@@ -40,11 +38,9 @@ ui <- fluidPage(
               the distribution name. If only one variable is included, do not input
               the second parameter'),
     h6('For Uniform distribution:'),
-    helpText('Param1 must be larger than Param2.'),
+    helpText('Parameter 2 must be larger than Parameter 1.'),
     h6('For Binomial distribution:'),
-    helpText('Param2 must be the decimal representation of the probability.'),
-    h6('For Logarithmic distribution:'),
-    helpText('Param2 must be the decimal representation of st. deviation.')
+    helpText('Parameter2 must be the decimal representation of the probability.')
   ),
   
   # plotting the distributions
